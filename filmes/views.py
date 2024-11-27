cd8ab821eb93752c350820c94c795af28e8a38da
Henrique Rios <114230619+RiqueRioss@users.noreply.github.com>
2024-11-27 14:08:48 -0300

    cosia

--- filmes/views.py
+++ filmes/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Filme, Review
 from django.contrib.auth.decorators import login_required
+from django.db.models import Avg
 
 def lista_filmes(request):
-    filmes = Filme.objects.all()
+    filmes = Filme.objects.annotate(media_notas=Avg('reviews__nota'))  # Calcula a média das notas
     return render(request, 'filmes/lista_filmes.html', {'filmes': filmes})
 
 def detalhe_filme(request, filme_id):
     filme = get_object_or_404(Filme, id=filme_id)
     reviews = filme.reviews.all()
-    return render(request, 'filmes/detalhe_filme.html', {'filme': filme, 'reviews': reviews})
+    media_notas = reviews.aggregate(media=Avg('nota'))['media']  # Calcula a média das notas
+
+    return render(request, 'filmes/detalhe_filme.html', {
+        'filme': filme,
+        'reviews': reviews,
+        'media_notas': media_notas,  # Passa a média para o template
+    })
 
 @login_required
 def adicionar_review(request, filme_id):
