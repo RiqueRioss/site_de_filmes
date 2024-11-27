94e66bf5ccc5b6dfa610a66fb45a6029a0ee0996
Henrique Rios <114230619+RiqueRioss@users.noreply.github.com>
2024-11-27 15:24:25 -0300

    da para adicionar filmes

--- filmes/urls.py
+++ filmes/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.lista_filmes, name='lista_filmes'),
     path('<int:filme_id>/', views.detalhe_filme, name='detalhe_filme'),
     path('<int:filme_id>/adicionar_review/', views.adicionar_review, name='adicionar_review'),
+
+    path('adicionar/', views.adicionar_filme, name='adicionar_filme'),  # URL para adicionar filmes
 ]
--- filmes/views.py
+++ filmes/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Filme, Review
 from django.contrib.auth.decorators import login_required
 from django.db.models import Avg
+from .forms import FilmeForm
 
 def lista_filmes(request):
     filmes = Filme.objects.annotate(media_notas=Avg('reviews__nota'))  # Calcula a média das notas
-    return render(request, 'filmes/lista_filmes.html', {'filmes': filmes})
+    return render(request, 'filmes/lista_filmes.html', {
+        'filmes': filmes,
+        'is_admin': request.user.is_staff  # Passa a informação se o usuário é admin
+    })
 
 def detalhe_filme(request, filme_id):
     filme = get_object_or_404(Filme, id=filme_id)
@@ -27,3 +31,19 @@ def adicionar_review(request, filme_id):
         Review.objects.create(usuario=request.user, filme=filme, nota=nota, comentario=comentario)
         return redirect('detalhe_filme', filme_id=filme.id)
     return render(request, 'filmes/adicionar_review.html', {'filme': filme})
+
+@login_required
+def adicionar_filme(request):
+    # Verifica se o usuário é um admin
+    if not request.user.is_staff:
+        return redirect('lista_filmes')  # Redireciona se não for admin
+
+    if request.method == 'POST':
+        form = FilmeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_filmes')  # Redireciona para a lista de filmes após salvar
+    else:
+        form = FilmeForm()
+
+    return render(request, 'filmes/adicionar_filme.html', {'form': form})
