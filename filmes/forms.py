e8cbf58d85ce91e6c3c9ae8890ec3d86cea78dc6
MatheusOMaia <matheusmaia.freire@gmail.com>
2024-11-29 00:38:38 -0300

    Versao finalizada

--- filmes/forms.py
+++ filmes/forms.py
@@ -10,3 +10,15 @@ class FilmeSegundaListaForm(forms.ModelForm):
     class Meta:
         model = FilmeSegundaLista
         fields = ['titulo', 'descricao', 'imagem']
+
+        def __init__(self, *args, **kwargs):
+            super(FilmeSegundaListaForm, self).__init__(*args, **kwargs)
+            
+            # Adicionando a classe ao label de 'titulo'
+            self.fields['titulo'].label_tag(attrs={'class': 'form-label'})
+            
+            # Adicionando a classe ao label de 'descricao'
+            self.fields['descricao'].label_tag(attrs={'class': 'form-label'})
+            
+            # Adicionando a classe ao label de 'imagem'
+            self.fields['imagem'].label_tag(attrs={'class': 'form-label'})
